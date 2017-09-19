24c641d4e97a80bb47af16f8695ef8e4b4484838
wanwu <442629928@qq.com>
2017-09-19 16:57:22 +0800

    daily updated

---
+++ DataHandlerApplication/FileToValidTSV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace DataHandlerApplication
+{
+   public class FileToValidTSV
+   {
+      public void convertToValidTSV(string inputPath,string outputPath,int colNums)
+      {
+         int originalLineNum = 0, generateLineNum = 0;
+         StreamWriter sw = new StreamWriter(outputPath);
+         var content = File.ReadAllLines(inputPath);
+         foreach (var items in content)
+         {
+            originalLineNum++;
+            string[] cols = items.Split('\t');
+            if (cols.Length != colNums || !OtherVerifyCondition(items)) continue;
+            sw.WriteLine(items);
+            generateLineNum++;
+         }
+         Console.WriteLine("original line number:" + originalLineNum + " generate line number:" + generateLineNum);
+         Console.ReadLine();
+         sw.Close();
+      }
+      public virtual bool OtherVerifyCondition(string line) { return true; }
+   }
+}
---
+++ DataHandlerApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandlerApplication
+{
+   class Program
+   {
+      public static void queryFilter()
+      {
+         string input = @"D:\News_Team\Query-author-in-Twitter\QueryTopSites.txt";
+         string output = @"D:\News_Team\Query-author-in-Twitter\QueryTopSites.tsv";
+         new FileToValidTSV().convertToValidTSV(input, output, 4);
+      }
+      static void Main(string[] args)
+      {
+         queryFilter();
+      }
+   }
+}
