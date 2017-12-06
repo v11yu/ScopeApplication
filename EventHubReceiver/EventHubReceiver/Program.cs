e1114ed56fddf3e55b3f777a4b77aed03d2bb16a
v11yu <442629928@qq.com>
2017-12-06 11:48:12 +0000

    eventhub demo
    11

---
+++ EventHubReceiver/EventHubReceiver/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.Diagnostics;
+
+namespace EventHubReceiver
+{
+    class SimpleEventProcessor : IEventProcessor
+    {
+        Stopwatch checkpointStopWatch;
+
+        async Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        Task IEventProcessor.OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("SimpleEventProcessor initialized.  Partition: '{0}', Offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+            this.checkpointStopWatch = new Stopwatch();
+            this.checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (EventData eventData in messages)
+            {
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+
+                Console.WriteLine(string.Format("Message received.  Partition: '{0}', Data: '{1}'",
+                    context.Lease.PartitionId, data));
+            }
+
+            //Call checkpoint every 5 minutes, so that worker can resume processing from 5 minutes back if it restarts.
+            if (this.checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
+            {
+                await context.CheckpointAsync();
+                this.checkpointStopWatch.Restart();
+            }
+        }
+    
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://v11-test.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=vsGHhkWdKA2uukXjUmgmRpMcsnj2315HOZhd3EuWw/8=";
+            string eventHubName = "wanwutest";
+            string storageAccountName = "v11";
+            string storageAccountKey = "e41T5FjeDOyYgAE23HAgBhoOeZVnlm/ITbVz2mhiUtF8yOq43tJXly1VxWfaGb0v5KK5+E0oe3eZBA/LNYvu3Q==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            //var options = new EventProcessorOptions();
+            //options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
